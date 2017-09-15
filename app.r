84be9651a82ee1fd0b5630ec0f9e633e8dd12799
noorykim <noorykim@users.noreply.github.com>
2017-09-15 11:03:06 -0400

    Add files via upload

--- app.r
+++ app.r
@@ -1,97 +1,120 @@
 library(shiny)
 library(SAMURAI)
+
+## load data sets
 data(greentea)
 data(Hpylori)
 data(Fleiss1993)
 
-# Define UI for app that draws a histogram ----
+## Define UI for app
 ui <- fluidPage(
   
-  # App title ----
-  titlePanel("Sensitivity Analysis of a Meta-Analytic Effect Size"),
+  ## app title
+  tags$h1("Sensitivity Analysis of a Meta-Analysis to Unpublished Studies"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+  fluidRow(
+    column(12, 
+      tags$h4('References:'),
+      tags$a(href='https://doi.org/10.1186/2046-4053-3-27', 'Systematic Reviews 2014 3:27'),
+      br(),
+      tags$a(href='https://cran.r-project.org/web/packages/SAMURAI/index.html', 'SAMURAI, an R package')
+    )
+  ),
+  
+  ## inputs
+  fluidRow(
+    column(12, 
+      ## Choose dataset 
+      selectInput("dataset", 
+                  label = h4("Choose a dataset:"),
+                  choices = c("green tea", "Fleiss (1993)", "H.pylori"),
+                  width = '40%')
+    )  
+  ),
 
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Choose dataset ----
-      selectInput("dataset", "Choose a dataset:",
-                  choices = c("greentea", "Hpylori")
-      ),      
-            
-      # Copy the line below to make a select box 
+  fluidRow(
+    column(6, 
+      ## select box 1
       selectInput(
         "studies_outlook1", 
-        label = h4("Select outlook of unpublished studies in Plot 1"), 
-        choices = list("reset",
+        label = h4("Select outlook of all unpublished studies in Plot 1"), 
+        choices = list("initial settings",
                        "-- --",
                        "very positive", "positive", "no effect", "negative", "very negative", 
-                       "-- based on published studies --",
+                       "-- based on published studies' effect size and CL --",
                        "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
-        selected = 1
-      ),
-  
+        selected = "initial settings",
+        width = '100%')
+    ),
+    column(6,
+      # select box 2
       selectInput(
         "studies_outlook2", 
-        label = h4("Select outlook of unpublished studies in Plot 2"), 
-        choices = list("reset",
+        label = h4("Select outlook of all unpublished studies in Plot 2"), 
+        choices = list("initial settings",
                        "-- --",
                        "very positive", "positive", "no effect", "negative", "very negative", 
-                       "-- based on published studies --",
+                       "-- based on published studies' effect size and CL --",
                        "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
-        selected = 1
+        selected = "initial settings",
+        width = '100%')    
       )
     
-    # actionButton(inputId = "reset", label = "Reset")  
-      
-    ),
+    # actionButton(inputId = "initial settings", label = "initial settings")  
+  ),
     
-    # Main panel for displaying outputs ----
-    mainPanel(
-
+  ## display outputs 
+  fluidRow(
+    column(12,
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(type = "tabs",
                   tabPanel("Plot 1", plotOutput(outputId = "plot1")),
                   tabPanel("Plot 2", plotOutput(outputId = "plot2"))
-      )      
-      
-    )
+                  )      
+    )  
   )
+ 
 )
 
-# Define server logic required to draw a histogram ----
+## Define server logic for app
 server <- function(input, output) {
   
-  # Reactive value for selected dataset ----
+  ## Reactive values for dataset selected
+  
   inputData <- reactive({
     switch(input$dataset,
-           "greentea" = greentea,
-           "Hpylori" = Hpylori)
+           "green tea" = greentea,
+           "H.pylori" = Hpylori,
+           "Fleiss (1993)" = Fleiss1993)
   })  
   
   qbinary <- reactive({
     switch(input$dataset,
-           "greentea" = FALSE,
-           "Hpylori" = TRUE)
+           "green tea" = FALSE,
+           "H.pylori" = TRUE,
+           "Fleiss (1993)" = TRUE)
   })  
 
   qmean.sd <- reactive({
     switch(input$dataset,
-           "greentea" = TRUE,
-           "Hpylori" = FALSE)
+           "green tea" = TRUE,
+           "H.pylori" = FALSE,
+           "Fleiss (1993)" = FALSE)
   })
   
   qhigher.is.better <- reactive({
     switch(input$dataset,
-           "greentea" = FALSE,
-           "Hpylori" = FALSE)
+           "green tea" = FALSE,
+           "H.pylori" = FALSE,
+           "Fleiss (1993)" = FALSE)
   })
+
+    
+  ## render plots
   
   output$plot1 <- renderPlot({
     
-    if (input$studies_outlook1 != "reset" && substring(input$studies_outlook1, 1, 1) != '-' ){
+    if (input$studies_outlook1 != "initial settings" && substring(input$studies_outlook1, 1, 1) != '-' ){
       uoutlook <- input$studies_outlook1
     }
     else {
@@ -108,7 +131,7 @@ server <- function(input, output) {
 
   output$plot2 <- renderPlot({
     
-    if (input$studies_outlook2 != "reset" && substring(input$studies_outlook2, 1, 1) != '-' ){
+    if (input$studies_outlook2 != "initial settings" && substring(input$studies_outlook2, 1, 1) != '-' ){
       uoutlook <- input$studies_outlook2
     }
     else{
@@ -125,7 +148,7 @@ server <- function(input, output) {
   
     
   # observeEvent(
-  #   input$reset,
+  #   input$initial settings,
   #   { output$plot <- renderPlot({forestsens(greentea,
   #                           binary=FALSE,
   #                           mean.sd=TRUE,
