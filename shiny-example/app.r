3fb4cca100851b67f677c54cf1eee2f717391ebb
noorykim <noorykim@users.noreply.github.com>
2017-09-18 20:51:08 -0400

    Add files via upload

--- shiny-example/app.r
+++ shiny-example/app.r
@@ -13,63 +13,63 @@ ui <- fluidPage(
   tags$h1("Sensitivity Analysis of a Meta-Analysis to Unpublished Studies"),
   
   fluidRow(
-
+    
     # sidebar of inputs 
     column(4,
-      wellPanel(
-        
-        ## select existing dataset
-        selectInput("dataset", 
-                    label = "Choose an existing dataset:",
-                    choices = c("green tea", "Fleiss (1993)", "H.pylori"),
-                    width = '100%'),       
-  
-        ## Choose dataset to download
-        downloadButton("downloadData", "Download as a CSV file"),
-        br(),
-        tags$a(href='https://noory.shinyapps.io/samurai/', 'Click here to upload your own dataset (coming soon)')
-      ),
-        
-      wellPanel(                
-        ## select box 1
-        selectInput("studies_outlook1", 
-                    label = "Select outlook of all unpublished studies in Plot 1", 
-                    choices = list("initial settings",
-                                   "-- --",
-                                   "very positive", "positive", "no effect", "negative", "very negative", 
-                                   "-- based on published studies' effect size and CL --",
-                                   "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
-                    selected = "initial settings",
-                    width = '100%'),
-        
-        ## select box 2
-        selectInput("studies_outlook2", 
-                    label = "Select outlook of all unpublished studies in Plot 2", 
-                    choices = list("initial settings",
-                                   "-- --",
-                                   "very positive", "positive", "no effect", "negative", "very negative", 
-                                   "-- based on published studies' effect size and CL --",
-                                   "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
-                    selected = "initial settings",
-                    width = '100%')
-      ),
-      
-      wellPanel(           
-        ## References
-        tags$h4('References:'),
-        tags$a(href='https://doi.org/10.1186/2046-4053-3-27', 'Systematic Reviews (2014) 3:27'),
-        br(),
-        tags$a(href='https://cran.r-project.org/web/packages/SAMURAI/index.html', 'SAMURAI, an R package')
-      )
-      
+           wellPanel(
+             
+             ## select existing dataset
+             selectInput("dataset", 
+                         label = "Choose an existing dataset:",
+                         choices = c("green tea", "Fleiss (1993)", "H.pylori"),
+                         width = '100%'),       
+             
+             ## Choose dataset to download
+             downloadButton("downloadData", "Download as a CSV file"),
+             br(),
+             tags$a(href='https://noory.shinyapps.io/samurai/', 'Click here to upload your own dataset (coming soon)')
+           ),
+           
+           wellPanel(                
+             ## select box 1
+             selectInput("studies_outlook1", 
+                         label = "Select outlook of all unpublished studies in Plot 1", 
+                         choices = list("initial settings",
+                                        "-- --",
+                                        "very positive", "positive", "no effect", "negative", "very negative", 
+                                        "-- based on published studies' effect size and CL --",
+                                        "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
+                         selected = "initial settings",
+                         width = '100%'),
+             
+             ## select box 2
+             selectInput("studies_outlook2", 
+                         label = "Select outlook of all unpublished studies in Plot 2", 
+                         choices = list("initial settings",
+                                        "-- --",
+                                        "very positive", "positive", "no effect", "negative", "very negative", 
+                                        "-- based on published studies' effect size and CL --",
+                                        "very positive CL", "positive CL", "current effect", "negative CL", "very negative CL"), 
+                         selected = "initial settings",
+                         width = '100%')
+           ),
+           
+           wellPanel(           
+             ## References
+             tags$h4('References:'),
+             tags$a(href='https://doi.org/10.1186/2046-4053-3-27', 'Systematic Reviews (2014) 3:27'),
+             br(),
+             tags$a(href='https://cran.r-project.org/web/packages/SAMURAI/index.html', 'SAMURAI, an R package')
+           )
+           
     ),
     
     ## display outputs 
     column(8,
-        # Output: Tabset w/ plot, summary, and table ----
-        tabsetPanel(type = "tabs",
-                    tabPanel("Click here for Plot 1", plotOutput(outputId = "plot1")),
-                    tabPanel("Click here for Plot 2", plotOutput(outputId = "plot2")) )      
+           # Output: Tabset w/ plot, summary, and table ----
+           tabsetPanel(type = "tabs",
+                       tabPanel("Click here for Plot 1", plotOutput(outputId = "plot1")),
+                       tabPanel("Click here for Plot 2", plotOutput(outputId = "plot2")) )      
     )  
     
   )
@@ -86,14 +86,6 @@ server <- function(input, output) {
            "H.pylori" = Hpylori,
            "Fleiss (1993)" = Fleiss1993)
   })  
-  
-  inputDataName <- reactive({
-    switch(input$dataset,
-           " " = "greentea",
-           "green tea" = "greentea",
-           "H.pylori" = "Hpylori",
-           "Fleiss (1993)" = "Fleiss1993")
-  })
   
   qbinary <- reactive({
     switch(input$dataset,
@@ -156,23 +148,12 @@ server <- function(input, output) {
   # Downloadable csv of selected dataset ----
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste(inputDataName(), ".csv", sep = "")
+      paste(gsub(' ', '', gsub('.', '', input$dataset)), ".csv", sep = "")
     },
     content = function(file) {
       write.csv(inputData(), file, row.names = FALSE)
     }
   )
-  
-  
-  # observeEvent(
-  #   input$initial settings,
-  #   { output$plot <- renderPlot({forestsens(greentea,
-  #                           binary=FALSE,
-  #                           mean.sd=TRUE,
-  #                           higher.is.better=FALSE,
-  #                           outlook = NA)})
-  #   }
-  # )
   
 }
 
